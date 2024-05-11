27daaf836baecf7a23f465f1c4152fd473d58e23
Sanjay <72803056+saisanjaypotluri@users.noreply.github.com>
2024-05-11 17:07:53 -0400

    Add files via upload

---
+++ dbo.Products.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Products] (
+[ProductID] INT IDENTITY (1, 1) NOT NULL,
+[ProductTab] NVARCHAR (30) NOT NULL,
+[ProductName] NVARCHAR (50) NULL,
+[ImageFile] NVARCHAR (30) NULL,
+[UnitPrice] DECIMAL (18, 2) NOT NULL,
+[MaxAmount] INT NOT NULL,
+[DefaultAmount] INT NOT NULL,
+PRIMARY KEY CLUSTERED ([ProductID] ASC)
+);
